ad353fd055d8f864578bd15d3975ee97eda291d6
jilei-hao <jilei.cs@gmail.com>
2023-10-09 20:09:19 -0400

    fixed authentication error

--- util/auth_middleware.js
+++ util/auth_middleware.js
@@ -4,7 +4,7 @@ require('dotenv').config();
 const secreteKey = process.env.SECRET_KEY;
 
 function authenticateToken(req, res, next) {
-  const authHeader = req.headers.authorization;
+  const authHeader = req.headers.authentication;
   const token = authHeader && authHeader.split(' ')[1];
 
   if (!token) {
@@ -15,6 +15,8 @@ function authenticateToken(req, res, next) {
     if (err) {
       return res.status(403).json({ error: 'Invalid token' });
     }
+
+    console.log("[authenticateToken] user: ", user);
 
     req.user = user;
     next();
